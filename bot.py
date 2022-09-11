d0be9c83addc759a19393b064d53eaa8cb7ad272
G_Mine <65339730+G-Mine2022@users.noreply.github.com>
2022-09-11 09:57:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,13 +8,20 @@ server.connect()
 intents = discord.Intents.all()
 TOKEN = "token" #put your token here
 
-channel = 00000 #channel id
+channel_cmd = 00000 #channel id
+channel_send = 00000 #channel id
+
 bot = commands.Bot(command_prefix=('/'), intents=intents)
 bot.remove_command( 'help' )
 
 @bot.command()
-async def cmd(ctx, cmd):
-    if ctx.channel.id == channel:
-        await ctx.send('Server output:\n'+server.command(cmd.replace('-', ' ')))#command usage: /cmd say-123
+async def cmd(ctx, *,cmd):
+    if ctx.channel.id == channel_cmd:
+        await ctx.send('Server output:\n'+server.command(cmd))#command usage: /cmd say 123
+@bot.command()
+async def send(ctx, ch_id, *, message):
+    if ctx.channel.id == channel_send:
+        channel = bot.get_channel(int(ch_id))
+        await channel.send(message)
 
 bot.run(TOKEN)
